cad752f1ac468a5fd563b54d1b310c7d41c8ff26
ijerkovic <igor@calimero.network>
2022-06-13 11:53:17 +0200

    [bridge] Add light client block method
    Implemented the add_light_client_block method, what still needs to be done is
    implementing hashing of the block producers in order for a require to be satisfied.
    
    Uncommented tests for add_light_client_block, but more will be added.
    
    Test plan:
    ./build.sh
    cargo test

--- contracts/light_client/src/lib.rs
+++ contracts/light_client/src/lib.rs
@@ -6,11 +6,12 @@ pub mod utils;
 pub use crate::utils::{hashes, u128_dec_format};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::serde::{Deserialize, Serialize};
-use near_sdk::{env, near_bindgen, require};
-use std::collections::HashMap;
+use near_sdk::{env, near_bindgen, require, PanicOnDefault, AccountId};
+use near_sdk::collections::LookupMap;
 
 // Current assumptions is that private shard only run max 100 block producers
 const MAX_BLOCK_PRODUCERS: u32 = 100;
+const NUM_OF_EPOCHS: usize = 3;
 
 pub type Hash = [u8; 32];
 
@@ -65,14 +66,14 @@ impl Block {
     }
 }
 
-#[derive(BorshDeserialize, BorshSerialize, Clone, Deserialize, Serialize, PartialEq, Eq)]
+// TODO: Signature type with related methods should be take from here:
+// https://github.com/near/nearcore/blob/master/core/crypto/src/signature.rs
+#[derive(BorshDeserialize, BorshSerialize, Clone, Deserialize, Serialize)]
 #[serde(crate = "near_sdk::serde")]
-pub enum Signature {
-    //ED25519(ed25519_dalek::Signature),
-    //SECP256K1(Secp256K1Signature),
-    ED25519(String),
-    SECP256K1(String),
+pub struct Signature {
+    pub signature: String,
 }
+
 #[derive(BorshDeserialize, BorshSerialize, Clone, Deserialize, Serialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct Validator {
@@ -134,20 +135,62 @@ pub struct Epoch {
 }
 
 #[near_bindgen]
-#[derive(Default, BorshDeserialize, BorshSerialize, Clone, Deserialize, Serialize)]
-#[serde(crate = "near_sdk::serde")]
+#[derive(PanicOnDefault, BorshDeserialize, BorshSerialize)]
 pub struct LightClient {
-    is_initialized: bool,
     epochs: Vec<Epoch>,
-    cur_height: u64,
-    block_hashes: HashMap<u64, Hash>,
-    block_merkle_roots: HashMap<u64, Hash>,
+    last_valid_at: u64,
+    current_height: u64,
+    // The most recently added block. May still be in its challenge period, so should not be trusted.
+    untrusted_height: u64,
+    // Address of the account which submitted the last block.
+    last_submitter: AccountId,
+    // Whether the contract was initialized.
+    initialized: bool,
+    untrusted_next_epoch: bool,
+    untrusted_hash: Hash,
+    untrusted_merkle_root: Hash,
+    untrusted_next_hash: Hash,
+    untrusted_timestamp: u64,
+    untrusted_signature_set: u128,
+    untrusted_signatures: Vec<Signature>,
+    // lockDuration and replaceDuration shouldn't be extremely big, so adding them to an uint64 timestamp should not overflow uint128.
+    lock_duration: u64,
+    // replaceDuration is in nanoseconds, because it is a difference between NEAR timestamps.
+    replace_duration: u64,
+    current_epoch_index: usize,
+    block_hashes: LookupMap<u64, Hash>,
+    block_merkle_roots: LookupMap<u64, Hash>,
 }
 
 #[near_bindgen]
 impl LightClient {
+
+    #[init]
+    pub fn new(lock_duration: u64, replace_duration: u64) -> Self {
+        Self {
+            epochs: Vec::new(),
+            last_valid_at: 0,
+            current_height: 0,
+            untrusted_height: 0,
+            last_submitter: env::signer_account_id(),
+            initialized: false,
+            untrusted_next_epoch: false,
+            untrusted_hash: Default::default(),
+            untrusted_merkle_root: Default::default(),
+            untrusted_next_hash: Default::default(),
+            untrusted_timestamp: 0,
+            untrusted_signature_set: 0,
+            untrusted_signatures: Vec::new(),
+            lock_duration: lock_duration,
+            replace_duration: replace_duration,
+            current_epoch_index: 0,
+            block_hashes: LookupMap::new(b"h"),
+            block_merkle_roots: LookupMap::new(b"m")
+        }
+    }
+
     pub fn is_initialized(&self) -> bool {
-        return self.is_initialized;
+        return self.initialized;
     }
 
     /// The first part of initialization -- setting the validators of the current epoch.
@@ -156,13 +199,16 @@ impl LightClient {
             !self.is_initialized() && self.epochs.is_empty(),
             "Wrong initialization stage"
         );
-        for _ in 1..3 {
+        for _ in 0..NUM_OF_EPOCHS {
             self.epochs.push(Epoch {
                 epoch_id: Default::default(),
                 keys: Vec::new(),
                 stake_threshold: 0,
                 stakes: Vec::new(),
             });
+        }
+        for _ in 0..MAX_BLOCK_PRODUCERS {
+            self.untrusted_signatures.push(Signature{signature: "".to_string()});
         }
         LightClient::set_block_producers(initial_validators, &mut self.epochs[0]);
     }
@@ -177,28 +223,105 @@ impl LightClient {
             block.next_bps.is_some(),
             "Initialization block must contain next_bps"
         );
+        self.initialized = true;
 
-        self.cur_height = block.inner_lite.height;
+        self.current_height = block.inner_lite.height;
         self.epochs[0].epoch_id = block.inner_lite.epoch_id;
         self.epochs[1].epoch_id = block.inner_lite.next_epoch_id;
         self.block_hashes
-            .insert(block.inner_lite.height, block.hash);
+            .insert(&block.inner_lite.height, &block.hash);
         self.block_merkle_roots
-            .insert(block.inner_lite.height, block.inner_lite.block_merkle_root);
+            .insert(&block.inner_lite.height, &block.inner_lite.block_merkle_root);
         LightClient::set_block_producers(block.next_bps.unwrap(), &mut self.epochs[1]);
     }
 
     pub fn block_hashes(&self, height: u64) -> Option<Hash> {
-        if let Some(res) = self.block_hashes.get(&height) {
+        if let Some(res) = &self.block_hashes.get(&height) {
             return Some(*res);
-        } else if env::block_timestamp() > 0 {
-            return None;
+        } else if env::block_timestamp() >= self.last_valid_at && self.last_valid_at != 0 && height == self.untrusted_height {
+            return Some(self.untrusted_hash);
         }
         return None;
     }
 
-    ///
-    pub fn add_light_client_block(&mut self, block: Block) {}
+    pub fn add_light_client_block(&mut self, block: Block) {
+        require!(self.is_initialized(), "Contract is not initialized");
+
+        // Commit the previous block, or make sure that it is OK to replace it.
+        if env::block_timestamp() < self.last_valid_at {
+            require!(block.inner_lite.timestamp >= self.untrusted_timestamp + self.replace_duration, "Can only replace with a sufficiently newer block");
+        } else if self.last_valid_at != 0 {
+            self.current_height = self.untrusted_height;
+            if self.untrusted_next_epoch {
+                self.current_epoch_index = (self.current_epoch_index + 1) % NUM_OF_EPOCHS;
+            }
+            self.last_valid_at = 0;
+
+            self.block_hashes.insert(&self.current_height, &self.untrusted_hash);
+            self.block_merkle_roots.insert(&self.current_height, &self.untrusted_merkle_root);
+        }
+
+        // Check that the new block's height is greater than the current one's.
+        require!(block.inner_lite.height > self.current_height, "New block must have higher height");
+
+        let from_next_epoch = if block.inner_lite.epoch_id == self.epochs[self.current_epoch_index].epoch_id { false }
+        else if block.inner_lite.epoch_id == self.epochs[(self.current_epoch_index + 1) % NUM_OF_EPOCHS].epoch_id { true }
+        else {
+            // in this case do a revert
+            require!(false, "Epoch id of the block is not valid");
+            false
+        };
+
+        // Check that the new block is signed by more than 2/3 of the validators.
+        let this_epoch = if from_next_epoch { &self.epochs[(self.current_epoch_index + 1) % NUM_OF_EPOCHS] } else { &self.epochs[self.current_epoch_index] };
+
+        // Last block in the epoch might contain extra approvals that light client can ignore.
+        require!(block.approvals_after_next.len() >= this_epoch.keys.len(), "Approval list is too short");
+
+        // The sum of uint128 values cannot overflow.
+        let mut voted_for: u128 = 0;
+        for i in 0..this_epoch.keys.len() {
+            if let Some(_) = block.approvals_after_next[i] {
+                voted_for += this_epoch.stakes[i];
+            }
+        }
+        require!(voted_for > this_epoch.stake_threshold, "Too few approvals");
+
+        // If the block is from the next epoch, make sure that next_bps is supplied and has a correct hash.
+        if from_next_epoch {
+            require!(block.next_bps.is_some(), "Next next_bps should not be None");
+            // TODO: Calculate hash of next block producers
+            // require(
+            //     HashOf(block.next_bps) == nearBlock.inner_lite.next_bp_hash,
+            //     "Hash of block producers does not match"
+            // );
+        }
+
+        self.untrusted_height = block.inner_lite.height;
+        self.untrusted_timestamp = block.inner_lite.timestamp;
+        self.untrusted_hash = block.hash;
+        self.untrusted_merkle_root = block.inner_lite.block_merkle_root;
+        self.untrusted_next_hash = block.next_hash;
+
+        let mut signature_set: u128 = 0;
+        let mut i = 0;
+        while i < this_epoch.keys.len() {
+            if let Some(approval) = block.approvals_after_next[i].clone() {
+                signature_set |= 1 << 1;
+                self.untrusted_signatures[i] = approval;
+            }
+            i = i + 1;
+        }
+        self.untrusted_signature_set = signature_set;
+        self.untrusted_next_epoch = from_next_epoch;
+        if from_next_epoch {
+            let mut next_epoch = &mut self.epochs[(self.current_epoch_index + 2) % NUM_OF_EPOCHS];
+            next_epoch.epoch_id = block.inner_lite.next_epoch_id;
+            LightClient::set_block_producers(block.next_bps.unwrap(), next_epoch);
+        }
+        self.last_submitter = env::predecessor_account_id();
+        self.last_valid_at = env::block_timestamp() + self.lock_duration;
+    }
 
     fn set_block_producers(block_producers: Vec<Validator>, epoch: &mut Epoch) {
         require!(
--- contracts/light_client/tests/client_test.rs
+++ contracts/light_client/tests/client_test.rs
@@ -2,15 +2,32 @@
 mod light_client {
     mod test {
         use light_client::hashes::encode_hex;
-        use light_client::{Block, BlockHeaderInnerLite, LightClient, Validator};
+        use light_client::{Block, BlockHeaderInnerLite, LightClient, Validator, Signature};
         use near_sdk::serde::de::DeserializeOwned;
         use near_sdk::serde_json::Value;
+        use near_sdk::test_utils::{accounts, VMContextBuilder};
+        use near_sdk::{testing_env, AccountId};
         use std::error::Error;
         use std::fs::File;
         use std::io::BufReader;
 
+        const TEST_BLOCK_TIMESTAMP_MULTIPLIER: u64 = 100000000;
+        const TEST_LOCK_DURATION: u64 = 10;
+        const TEST_REPLACE_DURATION: u64 = 20000000000;
+
+        fn get_context(predecessor_account_id: AccountId, block_timestamp: u64, block_index: u64) -> VMContextBuilder {
+            let mut builder = VMContextBuilder::new();
+            builder
+                .current_account_id(accounts(0))
+                .signer_account_id(predecessor_account_id.clone())
+                .predecessor_account_id(predecessor_account_id)
+                .block_timestamp(block_timestamp)
+                .block_index(block_index);
+            builder
+        }
+
         fn init() -> LightClient {
-            return Default::default();
+            LightClient::new(TEST_LOCK_DURATION, TEST_REPLACE_DURATION)
         }
 
         fn file_as_json<T: DeserializeOwned>(filename: &str) -> Result<T, Box<dyn Error>> {
@@ -32,6 +49,19 @@ mod light_client {
                 });
             }
 
+            return ret;
+        }
+
+        fn array_to_signatures(signatures: Option<&Vec<Value>>) -> Vec<Option<Signature>> {
+            let mut ret = Vec::<Option<Signature>>::new();
+            for item in signatures.unwrap() {
+                let val = item.as_str();
+                if val.is_none() {
+                    ret.push(None);
+                } else {
+                    ret.push(Some(Signature { signature: val.unwrap().to_string() }));
+                }
+            }
             return ret;
         }
 
@@ -59,12 +89,15 @@ mod light_client {
                 ),
                 String::from(block_value["inner_rest_hash"].as_str().unwrap()),
                 Some(array_to_validators(block_value["next_bps"].as_array())),
-                Vec::new(),
+                array_to_signatures(block_value["approvals_after_next"].as_array()),
             )
         }
 
         #[test]
         fn block_hashes() {
+            let mut context = get_context(accounts(0), 9605 * TEST_BLOCK_TIMESTAMP_MULTIPLIER, 9605);
+            testing_env!(context.build());
+
             let mut bridge = init();
             let block9605 = file_as_json::<Value>("./block_9605.json").unwrap();
             let block9610 = file_as_json::<Value>("./block_9610.json").unwrap();
@@ -79,10 +112,20 @@ mod light_client {
                     == "c4770276d5e782d847ea3ce0674894a572df3ea75b960ff57d66395df0eb2a34"
             );
 
+            //get_context(accounts(0), 9605 * TEST_BLOCK_TIMESTAMP_MULTIPLIER, 9605);
+            testing_env!(context
+                .block_timestamp(9610 * TEST_BLOCK_TIMESTAMP_MULTIPLIER)
+                .block_index(9610)
+                .build());
             bridge.add_light_client_block(value_to_block(&block9610));
 
-            // TODO uncomment when add_light_client_block will do something
-            //assert!(encode_hex(&bridge.block_hashes(9610).unwrap()) == "f28629da269e59f2494c6bf283e9e67dadaa1c1f753607650d21e5e5b916a0dc");
+            let some_future_block_index = 9620;
+            testing_env!(context
+                .block_timestamp(some_future_block_index * TEST_BLOCK_TIMESTAMP_MULTIPLIER)
+                .block_index(some_future_block_index)
+                .build());
+
+            assert!(encode_hex(&bridge.block_hashes(9610).unwrap()) == "f28629da269e59f2494c6bf283e9e67dadaa1c1f753607650d21e5e5b916a0dc");
         }
 
         #[test]
@@ -98,7 +141,12 @@ mod light_client {
 
             let approvals_after_next = block308["approvals_after_next"].as_array();
 
+            let context_244 = get_context(accounts(0), 244 * TEST_BLOCK_TIMESTAMP_MULTIPLIER, 244);
+            testing_env!(context_244.build());
             bridge.init_with_validators(initial_validators);
+
+            let context_304 = get_context(accounts(0), 304 * TEST_BLOCK_TIMESTAMP_MULTIPLIER, 304);
+            testing_env!(context_304.build());
             bridge.init_with_block(value_to_block(&block304));
 
             bridge.block_hashes(304);
@@ -107,10 +155,15 @@ mod light_client {
                     == "ea43feedc69d8df45d6afcb25cf428ab0ba8044dd818586e48979797f5f55a01"
             );
 
+            let context_308 = get_context(accounts(0), 308 * TEST_BLOCK_TIMESTAMP_MULTIPLIER, 308);
+            testing_env!(context_308.build());
             bridge.add_light_client_block(value_to_block(&block308));
 
+            let context_future_320 = get_context(accounts(0), 320 * TEST_BLOCK_TIMESTAMP_MULTIPLIER, 320);
+            testing_env!(context_future_320.build());
+
             // TODO uncomment when add_light_client_block will do something
-            //assert!(encode_hex(&bridge.block_hashes(304).unwrap()) == "92c231eb7719d7cc7598e7bc614bbd0eb0be3729b47a36ede4a66033aa5051d9");
+            assert!(encode_hex(&bridge.block_hashes(308).unwrap()) == "92c231eb7719d7cc7598e7bc614bbd0eb0be3729b47a36ede4a66033aa5051d9");
 
             for key_value in approvals_after_next.unwrap() {
                 if !key_value.is_null() {
