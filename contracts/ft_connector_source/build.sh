162e830dae43dadff12d4f15a5a23b5e068e0457
medicz <zvonimir.medic@gmail.com>
2022-08-04 15:09:00 +0200

    [Infra] Keep all wasms in one place

--- contracts/ft_connector_source/build.sh
+++ contracts/ft_connector_source/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 cargo build --target wasm32-unknown-unknown --release
+cp target/wasm32-unknown-unknown/release/ft_connector_source.wasm ../wasm/
--- contracts/prover/build.sh
+++ contracts/prover/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 cargo build --target wasm32-unknown-unknown --release
+cp target/wasm32-unknown-unknown/release/prover.wasm ../wasm/
