c48aaee616ffa9fb35dcb98f50a920dfe3504b0c
ijerkovic <igor.jerkovic@tolar.io>
2022-08-08 11:31:27 +0200

    Bridge CLI improvements
    Separated sending the tx from proving the tx.
    
    Test plan:
    Start relayers calimero < = > near testnet
    Transfer wNEAR from testnet to Calimero, and back.

--- contracts/ft_connector_destination/deploy_calimero.sh
+++ contracts/ft_connector_destination/deploy_calimero.sh
@@ -9,7 +9,6 @@ prover="p.$destination_master_account"
 near deploy \
   --accountId "connector.$destination_master_account" \
   --wasmFile target/wasm32-unknown-unknown/release/ft_connector_destination.wasm \
-  --initFunction new --initArgs  "{\"prover_account\":\"$prover\", \"source_master_account\": \"testnet\", \"destination_master_account\": \"$destination_master_account\"}" \
+  --initFunction new --initArgs "{\"prover_account\":\"$prover\", \"source_master_account\": \"testnet\", \"destination_master_account\": \"$destination_master_account\"}" \
   --nodeUrl "https://api-staging.calimero.network/api/v1/shards/$1/neard-rpc" \
   --networkId "$1"
-  
