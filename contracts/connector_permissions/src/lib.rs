114d594a48b711124d230829ea941eaa49827534
ijerkovic <igor@calimero.network>
2022-10-14 15:00:42 +0200

    [Connectors] Permissions contract for connectors
    This contract enables to deny certain accounts from using the ft and nft
    connectors. Also, for the cross shard connector, either calls to any
    contract or to a specific can be denied.
    
    Test plan:
    Added new tests:
    cargo test

---
+++ contracts/connector_permissions/src/lib.rs
@@ -0,0 +1,128 @@
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::{env, near_bindgen, require, AccountId, PanicOnDefault};
+use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::collections::LookupSet;
+
+#[derive(Debug, Copy, Clone, Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub enum ConnectorType {
+    FT = 0,
+    NFT = 1,
+    XSC = 2,
+}
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
+pub struct ConnectorPermissions {
+    /// The account of the ft connector
+    pub ft_connector_account: AccountId,
+    /// The account of the nft connector
+    pub nft_connector_account: AccountId,
+    /// The account of the cross shard connector
+    pub xsc_connector_account: AccountId,
+    /// set of accounts denied for bridging fts
+    pub deny_listed_accounts_for_bridging_fts: LookupSet<AccountId>,
+    /// set of accounts denied for bridging nfts
+    pub deny_listed_accounts_for_bridging_nfts: LookupSet<AccountId>,
+    /// set of accounts denied for making cross shard calls
+    pub deny_listed_accounts_for_cross_shard_calls: LookupSet<AccountId>,
+    /// set of accounts denied for making cross shard calls per contract id, the data
+    /// in the set is in form {contract_id}|{account_id}
+    pub deny_listed_account_per_contract_for_cross_shard_calls: LookupSet<String>,
+}
+
+#[near_bindgen]
+impl ConnectorPermissions {
+
+    #[init]
+    pub fn new(
+        ft_connector_account: AccountId,
+        nft_connector_account: AccountId,
+        xsc_connector_account: AccountId,
+    ) -> Self {
+        require!(!env::state_exists(), "Already initialized");
+        Self {
+            ft_connector_account,
+            nft_connector_account,
+            xsc_connector_account,
+            deny_listed_accounts_for_bridging_fts: LookupSet::new(b"dft".to_vec()),
+            deny_listed_accounts_for_bridging_nfts: LookupSet::new(b"dnft".to_vec()),
+            deny_listed_accounts_for_cross_shard_calls: LookupSet::new(b"dxsc".to_vec()),
+            deny_listed_account_per_contract_for_cross_shard_calls: LookupSet::new(b"dxscc".to_vec()),
+        }
+    }
+
+    fn connector_from_type(&self, connector_type: ConnectorType) -> &AccountId {
+        return match connector_type {
+            ConnectorType::FT => &self.ft_connector_account,
+            ConnectorType::NFT => &self.nft_connector_account,
+            ConnectorType::XSC => &self.xsc_connector_account,
+        };
+    }
+
+    /// returns true if account_id is not denied per connector type
+    pub fn can_bridge(&self, account_id: &AccountId, connector_type: ConnectorType) -> bool {
+        return match connector_type {
+            ConnectorType::FT => !self.deny_listed_accounts_for_bridging_fts.contains(&account_id),
+            ConnectorType::NFT => !self.deny_listed_accounts_for_bridging_nfts.contains(&account_id),
+            ConnectorType::XSC => !self.deny_listed_accounts_for_cross_shard_calls.contains(&account_id),
+        }
+    }
+
+    /// adds the account id to a denied list per connector type, can only be called by the corresponding connector
+    pub fn deny_bridge(&mut self, account_id: AccountId, connector_type: ConnectorType) -> bool {
+        assert_eq!(
+            env::predecessor_account_id(),
+            *self.connector_from_type(connector_type),
+            "Only corresponding connector can add accounts to the deny list"
+        );
+        return match connector_type {
+            ConnectorType::FT => self.deny_listed_accounts_for_bridging_fts.insert(&account_id),
+            ConnectorType::NFT => self.deny_listed_accounts_for_bridging_nfts.insert(&account_id),
+            ConnectorType::XSC => self.deny_listed_accounts_for_cross_shard_calls.insert(&account_id),
+        }
+
+    }
+
+    /// removes the account id from the denied list per connector type, can only be called by the corresponding connector
+    pub fn allow_bridge(&mut self, account_id: AccountId, connector_type: ConnectorType) -> bool {
+        assert_eq!(
+            env::predecessor_account_id(),
+            *self.connector_from_type(connector_type),
+            "Only corresponding connector can remove accounts from the deny list"
+        );
+        return match connector_type {
+            ConnectorType::FT => self.deny_listed_accounts_for_bridging_fts.remove(&account_id),
+            ConnectorType::NFT => self.deny_listed_accounts_for_bridging_nfts.remove(&account_id),
+            ConnectorType::XSC => self.deny_listed_accounts_for_cross_shard_calls.remove(&account_id),
+        }
+    }
+
+    /// adds the account_id|contract_id to a denied list, can only be called by cross shard calls connector contract
+    pub fn deny_cross_shard_call_per_contract(&mut self, account_id: AccountId, contract_id: AccountId) -> bool {
+        assert_eq!(
+            env::predecessor_account_id(),
+            self.xsc_connector_account,
+            "Only cross shard connector can add accounts to the deny list for specific contract id"
+        );
+        self.deny_listed_account_per_contract_for_cross_shard_calls.insert(&format!("{}|{}", account_id, contract_id))
+    }
+
+    /// removes the account_id|contract_id from the denied list, can only be called by cross shard calls connector contract
+    pub fn allow_cross_shard_call_per_contract(&mut self, account_id: AccountId, contract_id: AccountId) -> bool {
+        assert_eq!(
+            env::predecessor_account_id(),
+            self.xsc_connector_account,
+            "Only cross shard connector can remove accounts from the deny list for specific contract id"
+        );
+        self.deny_listed_account_per_contract_for_cross_shard_calls.remove(&format!("{}|{}", account_id, contract_id))
+    }
+
+    /// checks if account_id is denied to make cross shard contract calls and whether the account_id is denied from calling a specific contract id
+    pub fn can_make_cross_shard_call_for_contract(&self, account_id: &AccountId, contract_id: AccountId) -> bool {
+        if !self.can_bridge(&account_id, ConnectorType::XSC) {
+            return false;
+        }
+        return !self.deny_listed_account_per_contract_for_cross_shard_calls.contains(&format!("{}|{}", account_id, contract_id))
+    }
+}
---
+++ contracts/connector_permissions/tests/connector_permissions_tests.rs
@@ -0,0 +1,258 @@
+#[cfg(all(test, not(target_arch = "wasm32")))]
+mod connector_permissions {
+    mod test {
+        use near_sdk::serde_json;
+        use near_sdk::serde_json::json;
+        use near_units::{parse_gas};
+        use workspaces::prelude::*;
+        use workspaces::{network::Sandbox, Contract, Worker};
+        use connector_permissions::ConnectorType;
+
+        const FT_CONNECTOR_ACCOUNT_ID: &str =  "dev-1111111111111-11111111111111";
+        const NFT_CONNECTOR_ACCOUNT_ID: &str = "dev-2222222222222-22222222222222";
+        const XSC_CONNECTOR_ACCOUNT_ID: &str = "dev-3333333333333-33333333333333";
+        const ALICE_ACCOUNT_ID: &str =         "dev-4444444444444-44444444444444";
+        const BOB_ACCOUNT_ID: &str =           "dev-5555555555555-55555555555555";
+
+        async fn init() -> (Worker<Sandbox>, Contract) {
+            let worker = workspaces::sandbox().await.unwrap();
+
+            let tla1 = workspaces::AccountId::try_from(FT_CONNECTOR_ACCOUNT_ID.to_string()).unwrap();
+            let tla2 = workspaces::AccountId::try_from(NFT_CONNECTOR_ACCOUNT_ID.to_string()).unwrap();
+            let tla3 = workspaces::AccountId::try_from(XSC_CONNECTOR_ACCOUNT_ID.to_string()).unwrap();
+
+            let connector_permissions_wasm = std::fs::read(
+                "./target/wasm32-unknown-unknown/release/connector_permissions.wasm",
+            )
+                .unwrap();
+            let connector_permissions_contract = worker.dev_deploy(&connector_permissions_wasm).await.unwrap();
+            connector_permissions_contract
+                .call(&worker, "new")
+                .args_json(json!({
+                    "ft_connector_account": tla1,
+                    "nft_connector_account": tla2,
+                    "xsc_connector_account": tla3,
+                }))
+                .unwrap()
+                .transact()
+                .await
+                .unwrap();
+
+            (worker, connector_permissions_contract)
+        }
+
+        async fn test_add_remove_and_read_for_connector_type(worker: &Worker<Sandbox>, connector_permissions_contract: &Contract, connector_type: ConnectorType) {
+
+            let connector_str = match connector_type {
+                ConnectorType::FT => FT_CONNECTOR_ACCOUNT_ID,
+                ConnectorType::NFT => NFT_CONNECTOR_ACCOUNT_ID,
+                ConnectorType::XSC => XSC_CONNECTOR_ACCOUNT_ID
+            };
+
+            let sec = workspaces::types::SecretKey::from_seed(workspaces::types::KeyType::ED25519, format!("secret_key_ft_connector_{}", connector_str).as_str());
+            let tla = workspaces::AccountId::try_from(connector_str.to_string()).unwrap();
+            let connector_account = worker.create_tla(tla, sec).await.unwrap().unwrap();
+
+            let add_to_deny_list_result = connector_account.call(&worker, connector_permissions_contract.id(), "deny_bridge")
+                .args_json(json!({
+                    "account_id": ALICE_ACCOUNT_ID,
+                    "connector_type": connector_type,
+                }))
+                .unwrap()
+                .gas(parse_gas!("300 Tgas") as u64)
+                .transact()
+                .await
+                .unwrap();
+
+            assert!(add_to_deny_list_result.is_success());
+
+            // Alice is denied, should return false
+            let can_alice_bridge_result_1: bool = worker.view(
+                connector_permissions_contract.id(),
+                "can_bridge",
+                serde_json::to_vec(&serde_json::json!({
+                    "account_id": ALICE_ACCOUNT_ID,
+                    "connector_type": connector_type,
+                })).unwrap())
+                .await
+                .unwrap()
+                .json()
+                .unwrap();
+
+            assert_eq!(can_alice_bridge_result_1, false);
+
+            // Bob is not denied, should return true
+            let can_bob_bridge_result: bool = worker.view(
+                connector_permissions_contract.id(),
+                "can_bridge",
+                serde_json::to_vec(&serde_json::json!({
+                    "account_id": BOB_ACCOUNT_ID,
+                    "connector_type": connector_type,
+                })).unwrap())
+                .await
+                .unwrap()
+                .json()
+                .unwrap();
+
+            assert_eq!(can_bob_bridge_result, true);
+
+            let remove_from_deny_list_result = connector_account.call(&worker, connector_permissions_contract.id(), "allow_bridge")
+                .args_json(json!({
+                    "account_id": ALICE_ACCOUNT_ID,
+                    "connector_type": connector_type,
+                }))
+                .unwrap()
+                .gas(parse_gas!("300 Tgas") as u64)
+                .transact()
+                .await
+                .unwrap();
+
+            assert!(remove_from_deny_list_result.is_success());
+
+            // Alice is no longer denied, should return true
+            let can_alice_bridge_result_2: bool = worker.view(
+                connector_permissions_contract.id(),
+                "can_bridge",
+                serde_json::to_vec(&serde_json::json!({
+                    "account_id": ALICE_ACCOUNT_ID,
+                    "connector_type": connector_type,
+                })).unwrap())
+                .await
+                .unwrap()
+                .json()
+                .unwrap();
+
+            assert_eq!(can_alice_bridge_result_2, true);
+        }
+
+        #[tokio::test]
+        async fn test_deny_list_for_all_connector_types() {
+            let (worker, connector_permissions_contract) = init().await;
+            test_add_remove_and_read_for_connector_type(&worker, &connector_permissions_contract, ConnectorType::FT).await;
+            test_add_remove_and_read_for_connector_type(&worker, &connector_permissions_contract, ConnectorType::NFT).await;
+            test_add_remove_and_read_for_connector_type(&worker, &connector_permissions_contract, ConnectorType::XSC).await;
+        }
+
+        #[tokio::test]
+        #[should_panic]
+        async fn test_add_to_deny_list_by_non_connector_account() {
+            let (worker, connector_permissions_contract) = init().await;
+
+            let sec = workspaces::types::SecretKey::from_seed(workspaces::types::KeyType::ED25519, "secret_key_alice");
+            let tla = workspaces::AccountId::try_from(ALICE_ACCOUNT_ID.to_string()).unwrap();
+            let alice_account = worker.create_tla(tla, sec).await.unwrap().unwrap();
+
+            let result = alice_account.call(&worker, connector_permissions_contract.id(), "deny_bridge")
+                .args_json(json!({
+                    "account_id": alice_account.id(),
+                    "connector_type": ConnectorType::FT,
+                }))
+                .unwrap()
+                .gas(parse_gas!("300 Tgas") as u64)
+                .transact()
+                .await
+                .unwrap();
+
+            assert!(!result.is_success());
+        }
+
+        #[tokio::test]
+        async fn test_deny_account_for_contract() {
+            let (worker, connector_permissions_contract) = init().await;
+            //test_add_remove_and_read_for_connector_type(&worker, &connector_permissions_contract, ConnectorType::XSC);
+
+            let sec = workspaces::types::SecretKey::from_seed(workspaces::types::KeyType::ED25519, "secret_key_for_xsc");
+            let tla = workspaces::AccountId::try_from(XSC_CONNECTOR_ACCOUNT_ID.to_string()).unwrap();
+            let xsc_connector_account = worker.create_tla(tla, sec).await.unwrap().unwrap();
+
+            let add_to_deny_contract_list_result = xsc_connector_account.call(&worker, connector_permissions_contract.id(), "deny_cross_shard_call_per_contract")
+                .args_json(json!({
+                    "account_id": ALICE_ACCOUNT_ID,
+                    "contract_id": "forbidden.for.alice.testnet",
+                }))
+                .unwrap()
+                .gas(parse_gas!("300 Tgas") as u64)
+                .transact()
+                .await
+                .unwrap();
+            assert!(add_to_deny_contract_list_result.is_success());
+
+            let can_alice_call_contract_result: bool = worker.view(
+                connector_permissions_contract.id(),
+                "can_make_cross_shard_call_for_contract",
+                serde_json::to_vec(&serde_json::json!({
+                    "account_id": ALICE_ACCOUNT_ID,
+                    "contract_id": "forbidden.for.alice.testnet",
+                })).unwrap())
+                .await
+                .unwrap()
+                .json()
+                .unwrap();
+            assert_eq!(can_alice_call_contract_result, false);
+
+            let can_bob_call_contract_result: bool = worker.view(
+                connector_permissions_contract.id(),
+                "can_make_cross_shard_call_for_contract",
+                serde_json::to_vec(&serde_json::json!({
+                    "account_id": BOB_ACCOUNT_ID,
+                    "contract_id": "forbidden.for.alice.testnet",
+                })).unwrap())
+                .await
+                .unwrap()
+                .json()
+                .unwrap();
+            assert_eq!(can_bob_call_contract_result, true);
+
+            let remove_from_deny_contract_list_result = xsc_connector_account.call(&worker, connector_permissions_contract.id(), "allow_cross_shard_call_per_contract")
+                .args_json(json!({
+                    "account_id": ALICE_ACCOUNT_ID,
+                    "contract_id": "forbidden.for.alice.testnet",
+                }))
+                .unwrap()
+                .gas(parse_gas!("300 Tgas") as u64)
+                .transact()
+                .await
+                .unwrap();
+            assert!(remove_from_deny_contract_list_result.is_success());
+
+            let can_alice_call_contract_result: bool = worker.view(
+                connector_permissions_contract.id(),
+                "can_make_cross_shard_call_for_contract",
+                serde_json::to_vec(&serde_json::json!({
+                    "account_id": ALICE_ACCOUNT_ID,
+                    "contract_id": "forbidden.for.alice.testnet",
+                })).unwrap())
+                .await
+                .unwrap()
+                .json()
+                .unwrap();
+            assert_eq!(can_alice_call_contract_result, true);
+
+            let add_to_deny_list_result = xsc_connector_account.call(&worker, connector_permissions_contract.id(), "deny_bridge")
+                .args_json(json!({
+                    "account_id": ALICE_ACCOUNT_ID,
+                    "connector_type": ConnectorType::XSC,
+                }))
+                .unwrap()
+                .gas(parse_gas!("300 Tgas") as u64)
+                .transact()
+                .await
+                .unwrap();
+            assert!(add_to_deny_list_result.is_success());
+
+            // The call for specific contract should still return true if that account is denied for making any cross shard calls
+            let can_alice_call_contract_result: bool = worker.view(
+                connector_permissions_contract.id(),
+                "can_make_cross_shard_call_for_contract",
+                serde_json::to_vec(&serde_json::json!({
+                    "account_id": ALICE_ACCOUNT_ID,
+                    "contract_id": "forbidden.for.alice.testnet",
+                })).unwrap())
+                .await
+                .unwrap()
+                .json()
+                .unwrap();
+            assert_eq!(can_alice_call_contract_result, false);
+        }
+    }
+}
